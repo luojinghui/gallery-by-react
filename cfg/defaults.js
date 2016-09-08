4fe20d8311c8442ab3aaccc932ea20df560e58a2
luojinghui <939035243@qq.com>
2016-09-08 15:07:28 +0800

    alter style

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -62,7 +62,7 @@ function getDefaultModules() {
 
 module.exports = {
   srcPath: srcPath,
-  publicPath: '/assets/',
+  publicPath: '../dist/assets/',
   port: dfltPort,
   getDefaultModules: getDefaultModules
 };
